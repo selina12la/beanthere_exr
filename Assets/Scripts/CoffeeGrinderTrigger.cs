dd72d3d17f4ee6ffd65a32812b38bda0b610c335
Selina Lanz <selina.lanz@students.fhnw.ch>
2026-03-30 14:53:02 +0200

    coffee beans

---
+++ Assets/Scripts/CoffeeGrinderTrigger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+ 
+public class CoffeeGrinderTrigger : MonoBehaviour
+{
+    public ParticleSystem coffeeParticles;
+    public AudioSource grinderSound;
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("CoffeeBeans"))
+        {
+            coffeeParticles.Play();
+            grinderSound.Play();
+        }
+    }
+ 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("CoffeeBeans"))
+        {
+            coffeeParticles.Stop();
+            grinderSound.Stop();
+        }
+    }
+}
